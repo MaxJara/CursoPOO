6f72ba1abb64df69a7a463c0ae84205fc91a81c6
MaxJara <maxmata77@gmail.com>
2021-06-27 23:27:25 -0500

    Se actualizaron archivos

--- JAVA/Car.java
+++ JAVA/Car.java
@@ -4,7 +4,12 @@ class Car {
     String driver;
     Integer passegenger;
 
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+    }
+
     void printDataCar() {
-        System.out.println("License: " + license + " Driver: " + driver);
+        System.out.println("License: " + license + "Name Driver: " + driver.name);
     }
 }
