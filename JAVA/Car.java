faab6d8bde94a44e96167c287bf551009079ae45
MaxJara <maxmata77@gmail.com>
2021-06-28 13:08:49 -0500

    Se actualizo archivos .java y .js

--- JAVA/Car.java
+++ JAVA/Car.java
@@ -1,8 +1,8 @@
 class Car {
     Integer id;
     String license;
-    String driver;
-    Integer passegenger;
+    Account driver;
+    private Integer passegenger;
 
     public Car(String license, Account driver){
         this.license = license;
@@ -10,6 +10,23 @@ class Car {
     }
 
     void printDataCar() {
-        System.out.println("License: " + license + "Name Driver: " + driver.name);
+        if(passegenger != null){
+            System.out.println("License: " + license + " Name Driver: " + driver.name + " Passengers: " + passegenger);
+        }
+        
     }
+
+
+    public Integer getPassenger(){
+        return passegenger;
+    }
+
+    public void setPassenger(Integer passenger) {
+        if(passenger == 4){
+            this.passegenger = passenger;
+        }else{
+            System.out.println("Necesitas asignar 4 pasajeros");
+        }
+    }
+    
 }
