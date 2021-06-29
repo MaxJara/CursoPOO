011766689d7d560c7cb7f85b62f999639955b0b1
MaxJara <maxmata77@gmail.com>
2021-06-28 19:55:01 -0500

    Se modifico lo ultimo del curso

--- PHP/Car.php
+++ PHP/Car.php
@@ -4,7 +4,7 @@ class Car {
     public $id;
     public $license;
     public $driver;
-    public $passenger;
+    protected $passenger;
 
     public function __construct($license, $driver){
         $this->license = $license;
@@ -12,7 +12,59 @@ class Car {
     }
 
     public function printDataCar() {
-        echo "Licencia: $this->license Driver: ".$this->driver->name;
+        echo "
+            Licencia: $this->license 
+            Driver: {$this->driver->name} 
+            Número de pasajeros: $this->passenger
+            
+    
+        ";
     }
-}
+
+    public function getId() {
+        return $this->id;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function getLicense() {
+        return $this->license;
+    }
+    public function setLicense($license){
+        $this->license = $license;
+    }
+
+    public function getDriver() {
+        return $this->driver;
+    }
+    public function setDriver($driver){
+        $this->driver = $driver;
+    }
+
+    public function getPassenger() {
+        return $this->passengers;
+    }
+
+    public function setPassenger($passengers) {
+        
+        if ($passengers == 4) {
+            $this->passengers = $passengers;
+        }
+        else {
+            echo "Necesitas asignar 4 pasajeros <br>";
+        }
+    
+    }
+
+    public function printDataCar() {
+        echo "
+            Licencia: $this->license 
+            Driver: {$this->driver->name} 
+            Document: {$this->driver->document}
+            Número de pasajeros: $this->passengers
+            <br>
+        ";
+    }
+}   
 ?>
--- PHP/uberX.php
+++ PHP/uberX.php
@@ -10,6 +10,21 @@ class UberX extends Car {
         $this->model = $model;
     }
 
+    public function printDataCar() {
+        echo "
+            Licencia: $this->license 
+            Driver: {$this->driver->name} 
+            Document: {$this->driver->document}
+            Número de pasajeros: $this->passengers
+            Modelo: $this->model
+            Marca: $this->brand
+            <br>
+        ";
+    }
+    
+
+}
+
 
 }
 ?>
